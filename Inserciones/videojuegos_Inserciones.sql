aae3ca4f23c02365a632daecfd55a4eb671754b2
jjyanangomez <juanyasa_04@hotmail.com>
2024-03-05 23:12:04 -0500

    Insercion de videojuegos solo campos obligatorios

--- Inserciones/videojuegos_Inserciones.sql
+++ Inserciones/videojuegos_Inserciones.sql
@@ -5,4 +5,10 @@ values(117,'HALO','Juego de disparos',5),
 (120,'CYBERPUNK 2077','Juego de disparos',5),
 (121,'RESIDENT EVIL 2','Juego de disparos y terror',5);
 
+--Insercion de campos obligatorios
+insert into videojuegos(codigo, nombre, valoracion)
+values(122,'UNCHARTED 4: EL DESENLACE DEL LADRÓN',5),
+(123,'GOD OF WAR RAGNAROK',5),
+(124,'FORZA MOTORSPORT 4',4);
+
 select * from videojuegos;
