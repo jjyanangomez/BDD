c7fb5248a1964fd2c9209adb9ff7a0b0f8c64423
jjyanangomez <juanyasa_04@hotmail.com>
2024-03-05 20:16:20 -0500

    Scripts 5 y 6 de videojuegos y transacciones

---
+++ scripts/transacciones.sql
@@ -0,0 +1,10 @@
+--transacciones
+create table transacciones(
+	codigo int not null,
+	numero_cuenta char(5) not null,
+	monto money not null,
+	tipo char(1) not null,
+	fecha date not null,
+	hota time not null,
+	CONSTRAINT transacciones_pk Primary key(codigo)
+)
