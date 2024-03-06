f4d750438a4985ddba0b21d4b9849492e0f80bbd
jjyanangomez <juanyasa_04@hotmail.com>
2024-03-05 23:08:23 -0500

    Insercion de registros_entrada y videojuegos

---
+++ Inserciones/videojuegos_Inserciones.sql
@@ -0,0 +1,8 @@
+insert into videojuegos(codigo, nombre, descripcion, valoracion)
+values(117,'HALO','Juego de disparos',5),
+(118,'THE LAST OF US PARTE II','Juego de supervivencia',5),
+(119,'GTA V','Juego de disparos y rpg',5),
+(120,'CYBERPUNK 2077','Juego de disparos',5),
+(121,'RESIDENT EVIL 2','Juego de disparos y terror',5);
+
+select * from videojuegos;
